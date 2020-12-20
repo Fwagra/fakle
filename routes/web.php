a166e4a963e6f29fdbdfdad8c0cb916800e65c68
Fwagra <fabiocerqueiracw@gmail.com>
2020-12-20 20:53:25 +0100

    Tests - Launching a game

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Game;
+use App\Tile;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,30 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('launch', function(){
+
+    $game  = new Game;
+    $game->code = "TESTS";
+    $game->save();
+
+    for ($color=1; $color < 7; $color++) { 
+        for ($shape=1; $shape < 7; $shape++) { 
+            for ($i=0; $i < 3; $i++) { 
+                $tile = new Tile;
+                $tile->color = $color;
+                $tile->shape = $shape;
+                $tile->position_x = null;
+                $tile->position_y = null;
+                $tile->game_id = $game->id;
+                $tile->save();
+            }
+        }
+    }
+    $firstTile = $game->tiles()->inRandomOrder()->first();
+    $firstTile->position_x = 0;
+    $firstTile->position_y = 0;
+    $firstTile->save();
+    dump($firstTile);
 });
