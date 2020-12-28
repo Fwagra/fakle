2b0c37f1d939fd7538f6b2a0d46ea02615af0186
Fwagra <fabiocerqueiracw@gmail.com>
2020-12-28 19:09:44 +0100

    Database migrations for handling players order

---
+++ database/migrations/2020_12_28_175751_edit_player_and_game.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EditPlayerAndGame extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->integer('order');
+        });
+        Schema::table('games', function (Blueprint $table) {
+            $table->string('code', 100)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('players', function (Blueprint $table) {
+            $table->dropColumn('order');
+        });
+        Schema::table('games', function (Blueprint $table) {
+            $table->string('code', 5)->change();
+        });
+    }
+}
